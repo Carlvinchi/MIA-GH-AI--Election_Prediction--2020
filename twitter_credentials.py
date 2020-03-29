336c38f520d7689dbd95649b8034d975c5675e7d
MIA-GH <51687157+MIA-GH@users.noreply.github.com>
2020-03-29 16:18:01 +0100

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+"""
+Ths file contains the twitter credentials from the twitter application
+in the developer console
+"""
+
+ACCESS_TOKEN = "2242232880-qg12obA7UlVz5zaq8zgZP5AQ7O1gdaDxDP37KSZ"
+ACCESS_TOKEN_SECRET = "ap4xw7TppyUoDhwaFGROIok3S4d5E7GwBeJd7OXwNa1gl"
+CONSUMER_KEY = "AzqsmWP7qDoQ0VD4AW0XdgQs0"
+CONSUMER_SECRET = "f68S3zNI8rjtBgiFAZC89Zz9Uc5e6AjxeKN4EPsJipl5csw6FL"
